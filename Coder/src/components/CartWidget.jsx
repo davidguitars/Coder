11262a7e68a04637867227774d756c34fb70b007
David Urdaneta <davidguitars26@gmail.com>
2022-07-16 23:02:31 -0400

    Creando nuestros componentes

---
+++ Coder/src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <FontAwesomeIcon icon="fa-solid fa-cart-shopping-fast" />
+  )
+}
+
+export default CartWidget
---
+++ Coder/src/components/NavbarContent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+
+const NavbarContent = () => {
+  return (
+    <div className='navbarContainer'>
+ <button className='navbarContent'>Home</button>
+ <button className='navbarContent'>Quienes Somos</button>
+ <button className='navbarContent'>Contacto</button> 
+
+  </div>
+  )
+}
+
+export default NavbarContent
+
+
+
+
+
